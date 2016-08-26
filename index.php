84c93f54e33fd659281bddc4ec6050ed26f8405e
Pavel <pavelquevedo@outlook.com>
2016-08-25 20:50:19 -0600

    Se cambio la url del sitio

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 	
 	define('DS', DIRECTORY_SEPARATOR);
 	define('ROOT', realpath(dirname(__FILE__)) . DS);
-	define('URL', 'http://localhost/cursophp/');
+	define('URL', 'http://localhost/students_admin/');
 
 	require_once "Config/Autoload.php";
 	Config\Autoload::run();
