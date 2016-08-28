0b92eea89876376788f0dc0b76734e2af15b6980
Pavel <pavelquevedo@outlook.com>
2016-08-28 13:48:06 -0600

    Se agrego menu a layout principal

--- Views/template.php
+++ Views/template.php
@@ -15,6 +15,25 @@
 		<link rel="stylesheet" href="<?php echo URL; ?>Views/template/css/bootstrap.css">
 	</head>
 	<body>
+		<nav class="navbar navbar-inverse">
+		  <div class="container-fluid">
+		    <div class="navbar-header">
+		      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-2">
+		        <span class="sr-only">Toggle navigation</span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		      </button>
+		      <a class="navbar-brand" href="#">Students Admin</a>
+		    </div>
+		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-2">
+		      <ul class="nav navbar-nav">
+		        <li class="active"><a href="http://localhost/students_admin/">Estudiantes <span class="sr-only">(current)</span></a></li>
+		        <li><a href="http://localhost/students_admin/seccion/index">Secciones</a></li>
+		      </ul>
+		    </div>
+		  </div>
+		</nav>
 		<div class="col-md-2"></div>
 		<div class="col-md-8">
 <?php
