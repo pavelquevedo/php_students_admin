750578b1b77d2e4238add64546d2ff1774f477a2
Pavel <pavelquevedo@outlook.com>
2016-08-28 18:32:08 -0600

    Se soluciono conflicto de titulo

--- Views/template.php
+++ Views/template.php
@@ -24,11 +24,7 @@
 		        <span class="icon-bar"></span>
 		        <span class="icon-bar"></span>
 		      </button>
-<<<<<<< HEAD
 		      <a class="navbar-brand" href="#">Administrador de Estudiantes</a>
-=======
-		      <a class="navbar-brand" href="#">Students Admin Hola</a>
->>>>>>> origin/master
 		    </div>
 		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-2">
 		      <ul class="nav navbar-nav">
